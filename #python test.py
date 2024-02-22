2edb9fad36009d7858c0c425f7cc4a218498e988
Bola Yosry <bola.yosry@gmail.com>
2024-02-23 01:06:16 +0300

    added age and faculty features

--- #python test.py
+++ #python test.py
@@ -1,19 +1,23 @@
 #python test
 a=[]
 b=[]
+c=[]
+d=[]
 x=0
 q="yes"
 while (q=="yes"):
    a.append(input(f"please enter student name number {x+1} : "))
    b.append(input(f"please enter student id number {x+1}: "))
+   c.append(input(f"please enter student faculty {x+1}: "))
+   d.append(input(f"please enter student age {x+1}: "))
    q=input("do you want to add more students (yes/no)")
    ##print(q)
    while (q !="yes" and q!="no"):
         if (q !="yes" and q!="no"):
              q=input("please ansewr by yes or no only, do you want to add more students ? (yes/no)")
    x += 1
-print("    Name            ID")
+print("    Name            ID         faculty       age")
 for x in range (len(a)):
 #while(a[x] != 0):
-    print(f"{x+1}- {a[x]}", end="    ")
-    print(f"{b[x]}")
+    print(f"{x+1}- {a[x]}    {b[x]}    {c[x]}   {d[x]}")
+    
